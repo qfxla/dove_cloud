686506e44b2bfc68266f903263dc3d72844280e5
xiaoliang <2057401688@qq.com>
2021-08-19 14:27:06 +0800

    完善common模块

--- pigeon_cloud_common/src/main/java/com/dove/config/ConvertConfig.java
+++ pigeon_cloud_common/src/main/java/com/dove/config/ConvertConfig.java
@@ -22,19 +22,15 @@ import java.util.List;
 @Configuration
 public class ConvertConfig {
 
-    //HttpMessageConverter是用来处理request和response里的数据的
     @Bean
     public HttpMessageConverters customConverters() {
         MappingJackson2HttpMessageConverter mappingJackson2HttpMessageConverter = new MappingJackson2HttpMessageConverter();
-        //ObjectMapper可以从字符串、流或文件解析JSON，并创建Java对象或对象图来表示已解析的JSON
         ObjectMapper objectMapper = new ObjectMapper();
-        //用于帮助objectMapper注册，帮助实现自定义扩展模块的类和接口(使用objectapper . registermodule注册)
         SimpleModule simpleModule = new SimpleModule();
-        //JSON Long ==> String(因为Long类型数据直接传给前端，容易发生精度损失，统一处理）
+        //JSON Long ==> String
         simpleModule.addSerializer(Long.class, ToStringSerializer.instance);
         simpleModule.addSerializer(BigInteger.class, ToStringSerializer.instance);
         objectMapper.registerModule(simpleModule);
-        //设置日期格式
         objectMapper.setDateFormat(new SimpleDateFormat("yyyy-MM-dd HH:mm:ss"));
         mappingJackson2HttpMessageConverter.setObjectMapper(objectMapper);
         //设置中文编码格式
