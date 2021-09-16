0166dfd57780ab909c0e3fb8ea0b893c428a1fc4
韦家 <2057401688@qq.com>
2021-09-16 06:15:49 +0000

    删除文件 pigeon_cloud_authority/src

--- pigeon_cloud_authority/src/main/java/com/dove/authority/utils/CheckUtil.java
+++
@@ -1,8 +0,0 @@
-package com.dove.authority.utils;
-
-/**
- * @author run
- * @since 2021/3/19 23:51
- */
-public class CheckUtil {
-}
--- pigeon_cloud_authority/src/test/java/Generator.java
+++
@@ -1,8 +0,0 @@
-/**
- * @author 小亮
- * @date 2021/8/17  -  14:52
- */
-public class Generator {
-
-
-}
